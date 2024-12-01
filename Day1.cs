c003cf2bd55e8a029bfe60f7104adf6be82e7193
TheIndra55 <wuppie55@gmail.com>
2024-12-01 17:05:26 +0100

    Initial commit

---
+++ Day1.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode2024
+{
+    internal class Day1 : IDay
+    {
+        public object Part1(string[] input)
+        {
+            var locations = input.Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries));
+
+            var group1 = locations.Select(x => int.Parse(x[0])).OrderBy(x => x).ToList();
+            var group2 = locations.Select(x => int.Parse(x[1])).OrderBy(x => x).ToList();
+
+            var distance = group1.Select((x, i) => Math.Max(x, group2[i]) - Math.Min(x, group2[i])).Sum();
+
+            return distance;
+        }
+
+        public object Part2(string[] input)
+        {
+            var locations = input.Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries));
+
+            var group1 = locations.Select(x => int.Parse(x[0])).ToList();
+            var group2 = locations.Select(x => int.Parse(x[1])).ToList();
+
+            var score = group1.Select(x => x * group2.Count(y => y == x)).Sum();
+
+            return score;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+var input = File.ReadAllLines("input.txt");
+
+var type = Assembly.GetCallingAssembly().GetTypes().First(x => x.Name == "Day1");
+var day = (IDay)Activator.CreateInstance(type)!;
+
+Console.WriteLine(day.Part1(input));
+Console.WriteLine(day.Part2(input));
+
+interface IDay
+{
+    public object Part1(string[] input);
+    public object Part2(string[] input);
+}
